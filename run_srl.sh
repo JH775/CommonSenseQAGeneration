8dce3d2a15bda5194fd0770683e1f58e30d9463d
[ Judith Rosenke - Sch Compt Infor Jrosenke Dec Sys Engr - Undergraduate - cbaral ] <jrosenke@agave1.agave.rc.asu.edu>
2020-10-24 06:29:39 -0700

    Add genereated input and output files

---
+++ run_srl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+#SBATCH -n 4                        # number of cores
+#SBATCH -t 0-24:00                  # wall time (D-HH:MM)
+#SBATCH -A jrosenke             # Account hours will be pulled from (commented out with double # in front)
+#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err             # STDERR (%j = JobId)
+#SBATCH --mail-type=ALL             # Send a notification when the job starts, stops, or fails
+#SBATCH --mail-user=jrosenke@asu.edu # send-to address
+
+
+python -m allennlp.run predict ./data/qasrl_parser_elmo.tar.gz ./input/input_nrlQA_CONTEXT.txt --include-package nrl --predictor qasrl_parser --output-file ./out_nrlQA.txt
